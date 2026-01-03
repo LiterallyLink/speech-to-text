2bb80054d0ac6ebc540b49e149abf5ddefe0b188
Charmed <67402756+ever-nascent@users.noreply.github.com>
2026-01-03 12:53:58 -0800

    restart

--- src/stt_keyboard/gui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""GUI components for STT Keyboard"""
-
-from .tray_icon import TrayIcon
-from .settings_window import SettingsWindow
-from .model_downloader import ModelDownloaderDialog
-from .overlay_widget import OverlayWidget
-
-__all__ = ['TrayIcon', 'SettingsWindow', 'ModelDownloaderDialog', 'OverlayWidget']
